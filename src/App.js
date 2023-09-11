feb4c9c95e3f748303411fc1c493a14a604061d7
KimSeonHui <zero9657@naver.com>
2023-09-12 00:29:54 +0900

    ✨ feat : NFT 데이터 조회

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Card, CardContent, CardMedia, Container, Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+
+import axios from 'axios';
+import { techaMiya } from './util/getMetadata';
+import NFTItem from './components/NFTItem/NFTItem';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	console.log(techaMiya);
+	const [tokens, setTokens] = useState([]);
+
+	const getTokens = async () => {
+		const resultsTokens = [];
+
+		for (let i = 0; i < 30; i++) {
+			const url = await techaMiya.tokenURI(i);
+			const res = await axios.get(url);
+			resultsTokens.push(res.data);
+		}
+		setTokens(resultsTokens);
+	};
+
+	useEffect(() => {
+		getTokens();
+	}, []);
+
+	return (
+		<Container
+			sx={{
+				width: '100%',
+				height: '100%',
+				py: 4,
+				background: `linear-gradient(
+					to bottom,
+					#0c0931,
+					#1d1856,
+					#e80074,
+					#c200db,
+					#00cfe5
+				)`,
+			}}
+		>
+			<Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', mb: 6 }}>
+				<Typography variant="h3" color="#fff">
+					TECHA MIYA
+				</Typography>
+			</Box>
+			<Grid container spacing={2} sx={{ width: '100%' }}>
+				<Grid item xs={3}></Grid>
+				<Grid item xs={9}>
+					<Grid container spacing={2}>
+						{tokens.map((token, index) => (
+							<NFTItem key={index} image={token.image} name={token.name} />
+						))}
+					</Grid>
+				</Grid>
+			</Grid>
+		</Container>
+	);
 }
 
 export default App;
