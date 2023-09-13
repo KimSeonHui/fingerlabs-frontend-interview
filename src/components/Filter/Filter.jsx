d622d250ec4fca344124abb004d69186494553ab
KimSeonHui <zero9657@naver.com>
2023-09-13 22:29:45 +0900

    ✨ feat : 속성 체크 시 filter 추가

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -6,11 +6,25 @@ import {
 	FormControlLabel,
 	Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import data from '../../data/techmiya_traits.json';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
-function Filter({ name }) {
+function Filter({ name, filter, setFilter }) {
+	const [check, setCheck] = useState([]);
+	const handleChange = (e) => {
+		const value = e.target.value;
+		const isChecked = e.target.checked;
+
+		if (isChecked) {
+			setCheck((prev) => prev.concat(e.target.value));
+			setFilter((prevFilter) => [...prevFilter, { trait_type: name, value: value }]);
+		} else {
+			setCheck((prev) => prev.filter((item) => item !== value));
+			setFilter((prevFilter) => prevFilter.filter((item) => item.value !== value));
+		}
+	};
+
 	return (
 		<Accordion sx={{ background: 'transparent', color: '#fff', border: '1px solid #36cacc' }}>
 			<AccordionSummary
@@ -24,10 +38,17 @@ function Filter({ name }) {
 			<AccordionDetails>
 				{data[name].map((item, index) => (
 					<FormControlLabel
-						control={<Checkbox sx={{ color: '#36cacc' }} />}
+						control={
+							<Checkbox
+								sx={{ color: '#36cacc' }}
+								onChange={handleChange}
+								checked={check.includes(item)}
+							/>
+						}
 						key={index}
 						label={item}
 						sx={{ display: 'block' }}
+						value={item}
 					/>
 				))}
 			</AccordionDetails>
